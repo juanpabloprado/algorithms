d96c0696c3c07736fa62fabdfd8bf8fe5257492e
Juan Prado <juan@juanpabloprado.com>
2021-08-22 15:07:33 -0500

    Add quicksort

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+X
+(binary_search/IterativeBinarySearch.java,7/b/7b9ca30ef2f90fbd845c4a63b23fb2be1a06a929
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
---
+++ Sorting/quicksort/Sort.java
@@ -0,0 +1,34 @@
+package quicksort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sort {
+    static List<Integer> quicksort(List<Integer> list) {
+        if (list.size() <= 1) {
+            return list;
+        }
+        List<Integer> lessThanPivot = new ArrayList<>();
+        List<Integer> greaterThanPivot = new ArrayList<>();
+        int pivot = list.get(0);
+        int length = list.size();
+        for (int i = 1; i < length; i++) {
+            int currentValue = list.get(i);
+            if (currentValue <= pivot) {
+                lessThanPivot.add(currentValue);
+            } else {
+                greaterThanPivot.add(currentValue);
+            }
+        }
+        List<Integer> sortedList = new ArrayList<>(quicksort(lessThanPivot));
+        sortedList.add(pivot);
+        sortedList.addAll(quicksort(greaterThanPivot));
+        return sortedList;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(32, 100, 1, 2, 29, 28, 88, 3, 50, 67, 37, 1, 57, 20));
+        System.out.println(quicksort(list));
+    }
+}
